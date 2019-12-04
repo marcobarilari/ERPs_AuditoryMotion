cafd9bbd58e9cbc354479e8e122fd3c2c3286c68
Remi Gau <remi_gau@hotmail.com>
2019-12-04 18:03:12 +0100

    Prettify
     - remove extra indents
     - try to make everything fit in one screen width
     - remove some dead code

--- AudMotion_sEEG.m
+++ AudMotion_sEEG.m
@@ -2,7 +2,7 @@
 
 % Original Script Written by Sam Weiller to localize MT+/V5
 % Adapted by M.Rezk to localize MT/MST (Huk,2002)
-% simplify the ALocaliser fmri experiment for ERPs
+% Simplify the ALocaliser fmri experiment for ERPs
 
 clear all;
 clc
@@ -17,17 +17,19 @@ fprintf('Connected Device is %s \n\n',device);
 %% Start me up
 % Get the subject Name
 SubjName = input('Subject Name: ','s');
-       if isempty(SubjName)
-          SubjName = 'test';
-       end
+if isempty(SubjName)
+    SubjName = 'test';
+end
 
 fprintf('Auditory ERPs \n\n')
 
 %% Experiment Parametes
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Init_pause = 3;
-ISI = 1.5;                                                                      % Interstimulus Interval between events in the block.
-% MAKE gaussian distribution of ISI later on.
+
+% Interstimulus Interval between events in the block
+ISI = 1.5;
+% MAKE uniform distribution of ISI later on.
 % average is 1.5 (after 1s sound, 1s min gap and max 2s)
 
 
@@ -39,30 +41,50 @@ ISI = 1.5;                                                                      
 
 
 
-%stim_duration = 1;                                                             % Duration of 1 sound % NEED TO UPDATE IN THE LOOP
-%event_duration = stim_duration + ISI;                                          % 1 trial duration % NEED TO UPDATE IN THE LOOP
-numEvents = 120;                                                                % Number of trials
-percTarget = 10;                                                                % Percentage of trials as target
+%stim_duration = 1; % Duration of 1 sound % NEED TO UPDATE IN THE LOOP
+%event_duration = stim_duration + ISI; % 1 trial duration % NEED TO UPDATE IN THE LOOP
+
+numEvents = 120; % Number of trials
+percTarget = 10; % Percentage of trials as target
 % CALCULATE THE EXACT TIMING OF THE EXP AND PREPARE 4MIN AND 5 MIN
 freq = 44100;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                                               % 1 Cycle = one inward and outward motion together
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Experimental Design
-soundfiles = {'static','mot_LRRL', 'mot_RLLR', 'static_T',...
-              'mot_LRRL_T', 'mot_RLLR_T'};                                      % order IS important
-condition = {'static','motion','motion','static','motion','motion'};
+
+% order IS important
+
+soundfiles = {...
+    'static',...
+    'mot_LRRL', ...
+    'mot_RLLR', ...
+    'static_T', ...
+    'mot_LRRL_T', ...
+    'mot_RLLR_T'};
+
+numcondition = length(soundfiles);
+
+condition = {...
+    'static',...
+    'motion',...
+    'motion',...
+    'static',...
+    'motion',...
+    'motion'};
+
 isTarget = [0 0 0 1 1 1];
 
-[Event_names, Event_order]= getTrialSeq(numEvents, percTarget);                                %pseudorandomized events order: 2 MOTION + 1 static + 10% of targers
-%[rndstim_names, rndstim_order]= getTrialSeq(numEvents, percTarget);
-% [trial_seq_names,trial_seq]
-numEvents = length(Event_order);                                                %reassign it in case pseudorandomization provided less trial number
-numcondition = length(soundfiles);
+%pseudorandomized events order: 2 MOTION + 1 static + 10% of targers
+[Event_names, Event_order]= getTrialSeq(numEvents, percTarget);
+
+%reassign it in case pseudorandomization provided less trial number
+numEvents = length(Event_order);
+
 
 
 %% InitializePsychAudio;
 InitializePsychSound(1);
-%OPEN AUDIO PORTS
+
 % open audio port
 phandle = PsychPortAudio('Open',[],[],1,freq,2);
 
@@ -79,8 +101,8 @@ end
 
 
 
-
 %% TRIGGER - OR NOT TRIGGER - HOW TRIGGER WORKS
+
 if strcmp(device,'test')
     
     % press key
@@ -91,22 +113,17 @@ if strcmp(device,'test')
     end
     
     
-% TRIGGER EEG?
+    % TRIGGER EEG?
 elseif strcmp(device,'eeg')
     fprintf('Waiting For Trigger...');
-    
-    
-    
-    
+
     %%%%%%%%
     
     %INSERT TIRGGER HERE ???
     
-    %%%%%%%%  
-    
+    %%%%%%%%
 
 end
-
 
 fprintf('starting experiment \n');
 
@@ -121,11 +138,11 @@ targetTime   = [];
 responseKey  = [];
 responseTime = [];
 
-eventOnsets=zeros(1,numEvents);
-eventEnds=zeros(1,numEvents);
-eventDurations=zeros(1,numEvents);
-responses=zeros(1,numEvents);
-playTime = zeros(1,numEvents);
+eventOnsets =       zeros(1,numEvents);
+eventEnds =         zeros(1,numEvents);
+eventDurations =    zeros(1,numEvents);
+responses =         zeros(1,numEvents);
+playTime =          zeros(1,numEvents);
 
 
 
@@ -165,12 +182,12 @@ for iEvent = 1:15 %numEvents
     eventEnds(iEvent)=GetSecs-experimentStartTime;
     eventDurations(iEvent)=eventEnds(iEvent)-eventOnsets(iEvent);
     
-    WaitSecs(ISI);                                                             % either use WaitSecs(ISI) or below while loop
+    WaitSecs(ISI); % either use WaitSecs(ISI) or below while loop
     
     %below while loop does not work atm - 03.12.2019
-%     while eventDurations(iEvent)<(event_duration)                                % getting rid off possible delays -> wait in the while loop
-%     end                                                                          % for the exact length of stimulus + response gap = eventDuration
-%     
+    %     while eventDurations(iEvent)<(event_duration)                                % getting rid off possible delays -> wait in the while loop
+    %     end                                                                          % for the exact length of stimulus + response gap = eventDuration
+    %
     
     responses(iEvent) = responseCount ;
     timeLogger(iEvent).endTime = GetSecs - experimentStartTime;                 % Get the time for the block end
@@ -180,7 +197,7 @@ for iEvent = 1:15 %numEvents
     % add response pressed or not // what is the response button?
     % consider adding the ending of each sound, atm it's the event (sound +
     % response gap)
-    % consider adding WaitSec for ending? 
+    % consider adding WaitSec for ending?
 end
 
 
@@ -198,34 +215,54 @@ for i=1:length(timeLogger)
     durations(i,1) = timeLogger(i).length;
 end
 
+
+
 %% KeyPresses and Times
-% for i=length(responseKey):-1:2                                                   % responseKey gives a '1' in all frames where button was pressed, so one motor response = gives multiple consequitive '1' frames
-%     if responseKey(i-1)~=0                                                       % therefore, we need to cancel consequitive '1' frames after the first button press
-%         responseKey(i)=0;                                                        % we loop through the responses and remove '1's that are not preceeded by a zero
-%         responseTime(i)=0;                                                       % this way, we remove the additional 1s for the same button response
-%     end                                                                          % - The same concept for the responseTime
+% responseKey gives a '1' in all frames where button was pressed, so one motor response = gives 
+% multiple consequitive '1' frames therefore, we need to cancel consequitive '1' frames after the 
+% first button press we loop through the responses and remove '1's that are not preceeded by a zero
+% this way, we remove the additional 1s for the same button response
+% - The same concept for the responseTime
+% The same concept as responseKey adn responseTime.
+% Our Targets lasts 3 frames, to remove the TargetTime for the 2nd and 3rd frame
+% we remove targets that are preceeded by a non-zero value
+% that way, we have the time of the first frame only of the target
+
+% for i=length(responseKey):-1:2                                                    
+%     if responseKey(i-1)~=0                                                       
+%         responseKey(i)=0;                                                        
+%         responseTime(i)=0;                                                       
+%     end                                                                         
 % end
-% 
-% for i=length(targetTime):-1:2                                                   % The same concept as responseKey adn responseTime.
-%     if targetTime(i-1)~=0                                                       % Our Targets lasts 3 frames, to remove the TargetTime for the 2nd and 3rd frame
-%         targetTime(i)=0;                                                        % we remove targets that are preceeded by a non-zero value
-%     end                                                                          % that way, we have the time of the first frame only of the target
+%
+% for i=length(targetTime):-1:2                                                   
+%     if targetTime(i-1)~=0                                                       
+%         targetTime(i)=0;                                                        
+%     end                                                                          
 % end
-% 
-% responseKey  = responseKey(responseKey > 0);                                       % Remove zero elements from responseKey, responseTime, & targetTime
+
+% Remove zero elements from responseKey, responseTime, & targetTime
+% responseKey  = responseKey(responseKey > 0); 
 % responseTime = responseTime(responseTime > 0);
 % targetTime   = targetTime(targetTime > 0);
+
+
 
 %% Take the total exp time
 PsychPortAudio('Close',phandle);
 Experiment_duration = GetSecs - experimentStartTime;
 
+
+
 %% Save a mat Log file
 % Onsets & durations are saved in seconds.
-save(['logFileFull_',SubjName,'.mat']);
-save(['logFile_',SubjName,'.mat'], 'names','onsets','durations','ends','responseTime','responseKey','Experiment_duration','playTime');
+save(['logFileFull_', SubjName, '.mat']);
+save(['logFile_', SubjName, '.mat'], ...
+    'names', 'onsets', 'durations', 'ends', 'responseTime', ...
+    'responseKey', 'Experiment_duration', 'playTime');
+
 %targets? targetTime?
-%save(['logFile_',SubjName,'.mat'], 'names','onsets','durations','ends','targets','responseTime','responseKey','targetTime','Experiment_duration','playTime');
+%save(['logFile_',SubjName,'.mat'], 'names','onsets','durations','ends', ...
+%  'targets','responseTime','responseKey','targetTime','Experiment_duration','playTime');
 
 fprintf('Sequence IS OVER!!\n');
-%end
