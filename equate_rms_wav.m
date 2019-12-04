d281d79be3c6f792ef8f04f5322074598b63ea94
marcobarilari <marcobarilr@gmail.com>
2019-12-04 13:29:45 +0100

    more cleanings

--- equate_rms_wav.m
+++ equate_rms_wav.m
@@ -1,33 +1,34 @@
 function  equate_rms_wav()
 
+% input the folder with the audio files
 file_dir = 'input_earfish';
 
+% select the reference audio file
 reference_wav_fn = 'olivier_pnoise_250ms_Static_center.wav';
 
+% read the wav file in the target folder
 addpath(fullfile(pwd, file_dir));
-
 file_list = dir('input_earfish/*.wav');
 
+% select only the audio file that are not rms-ed already and rms them 
 for i = 1:size(file_list,1)
     
     if ~contains(file_list(i).name, 'rms')
+        
         target_wav_fn = file_list(i).name;
         fprintf('\n\n\n%s\n\n', target_wav_fn)
         runFunction (reference_wav_fn,target_wav_fn,file_dir)
+        
     end
 
 end
 
 end
 
-
 function runFunction(reference_wav_fn,target_wav_fn,file_dir)
 %% This Script takes a file (target_wav_fn) and equates its rms with
 % another reference audio file (reference_wav_fn) amd gives the equated 
 % wav file as an output ('final_wave.wav')
-
-%reference_wav_fn = 'R_L.wav';
-%target_wav_fn = 'L_R.wav';
 
 % Get the rms of the original sound
 [reference_wav , FS_reference]= audioread(reference_wav_fn); 
@@ -41,7 +42,6 @@ rms_target = rms(target_wav) ;
 disp('rms of the target wav file')
 disp(rms_target)
 
-
 % correct for the rms differences in each channel
 final_wave = [ target_wav(:,1)*(rms_reference(1)/rms_target(1)) ...
                target_wav(:,2)*(rms_reference(2)/rms_target(2))] ;
@@ -50,10 +50,9 @@ final_wave = [ target_wav(:,1)*(rms_reference(1)/rms_target(1)) ...
 rms_final = rms(final_wave);
 disp('rms of the final wav file')
 disp(rms_final)
-%wavwrite(new_wave,'new_wave.wav')
-%audiowrite(target_wav_fn,final_wave,FS_reference)
-%wavwrite(final_wave,FS_reference,16,['rms_',target_wav_fn])
+
 audiowrite(fullfile(file_dir, ['rms_',target_wav_fn]),final_wave,FS_reference)
+
 %% plot the reference wav and final wav files
 figure()
 subplot(2,1,1)
