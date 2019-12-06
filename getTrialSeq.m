71b23a13ddc5c35ec6ffd02678501a1c174f5b5d
marcobarilari <marcobarilr@gmail.com>
2019-12-06 10:11:57 +0100

    refactor get trials

--- getTrialSeq.m
+++ getTrialSeq.m
@@ -22,158 +22,23 @@ switch expLength
     
     case 1
         
-        numEvents = numEvents/3;
+        nChunks = 3;
         
-        numTargets = numTargets/3;
+        numEvents = numEvents/nChunks;
         
-        % calculate how many trials per condition dividing them in 3 chunks to help
-        % even randomization across experiment per condition
+        numTargets = numTargets/nChunks;
         
-        static = (numEvents-numTargets)/2;
-        mot_LRRL = (numEvents-numTargets)/4;
-        mot_RLLR = (numEvents-numTargets)/4;
-        static_T = numTargets/2;
-        mot_LRRL_T = numTargets/4;
-        mot_RLLR_T = numTargets/4;
+        trial_seq = pseudorand(numEvents, numTargets, nChunks, expLength);
         
-        % create the three chunks of condition and randomize them and check that:
-        % 1 - the target is not in the first trial
-        % 2 - two target are not consecutive
-        % 3 - there are no more them 3 same trials consecutive (less than that is impossible)
+    otherwise
         
-        d = 1; % counter for trial number
+        nChunks = 2;
         
-        while d < numEvents*3-2
-            
-            % create a sequence of trials that contains 1 thirs of all the trials
-            trial_seq = [ ...
-                repmat(ones,1,static),  repmat(2,1,mot_LRRL),   repmat(3,1,mot_RLLR), ...
-                repmat(4,1,static_T),   repmat(5,1,mot_LRRL_T), repmat(6,1,mot_RLLR_T)];
-            
-            % we shuffle each chunck and concatenate them
-            trial_seq = [ Shuffle(trial_seq), Shuffle(trial_seq), Shuffle(trial_seq) ];
-            
-            % scan through the trial sequence and checks all the conditions
-            while d < length(trial_seq)-2
-                
-                if d == 1 && trial_seq(d) > 3 % no targets in the first trial
-                    d = 1;
-                    break
-                elseif trial_seq(d) > 3 && trial_seq(d+1) > 3 % avoid 2 consecutive targets
-                    d = 1;
-                    break
-                    % avoid series 3 times the same conditions
-                elseif trial_seq(d) ==  trial_seq(d+1) && trial_seq(d+1) ==  trial_seq(d+2) && trial_seq(d+2) ==  trial_seq(d+3)
-                    d = 1;
-                    break
-                else
-                    d = d+1;
-                end
-            end
-        end
+        numEvents = numEvents/nChunks;
         
-    case 2
+        numTargets = numTargets/nChunks;
         
-        numEvents = numEvents/2;
-        
-        numTargets = numTargets/2;
-        
-        % calculate how many trials per condition dividing them in 3 chunks to help
-        % even randomization across experiment per condition
-        
-        static = (numEvents-numTargets)/2;
-        mot_LRRL = (numEvents-numTargets)/4;
-        mot_RLLR = (numEvents-numTargets)/4;
-        static_T = numTargets/2;
-        mot_LRRL_T = numTargets/4;
-        mot_RLLR_T = numTargets/4;
-        
-        % create the three chunks of condition and randomize them and check that:
-        % 1 - the target is not in the first trial
-        % 2 - two target are not consecutive
-        % 3 - there are no more them 3 same trials consecutive (less than that is impossible)
-        
-        d = 1; % counter for trial number
-        
-        while d < numEvents*2-2
-            
-            % create a sequence of trials that contains 1 thirs of all the trials
-            trial_seq = [ ...
-                repmat(ones,1,static),  repmat(2,1,mot_LRRL),   repmat(3,1,mot_RLLR), ...
-                repmat(4,1,static_T),   repmat(5,1,mot_LRRL_T), repmat(6,1,mot_RLLR_T)];
-            
-            % we shuffle each chunck and concatenate them
-            trial_seq = [ Shuffle(trial_seq), Shuffle(trial_seq) ];
-            
-            % scan through the trial sequence and checks all the conditions
-            while d < length(trial_seq)-2
-                
-                if d == 1 && trial_seq(d) > 3 % no targets in the first trial
-                    d = 1;
-                    break
-                elseif trial_seq(d) > 3 && trial_seq(d+1) > 3 % avoid 2 consecutive targets
-                    d = 1;
-                    break
-                    % avoid series 3 times the same conditions
-                elseif trial_seq(d) ==  trial_seq(d+1) && trial_seq(d+1) ==  trial_seq(d+2) && trial_seq(d+2) ==  trial_seq(d+3)
-                    d = 1;
-                    break
-                else
-                    d = d+1;
-                end
-            end
-        end
-        
-    case 3
-        
-        numEvents = numEvents/2;
-        
-        numTargets = numTargets/2;
-        
-        % calculate how many trials per condition dividing them in 3 chunks to help
-        % even randomization across experiment per condition
-        
-        static = (numEvents-numTargets)/2;
-        mot_LRRL = (numEvents-numTargets)/4;
-        mot_RLLR = (numEvents-numTargets)/4;
-        static_T = numTargets/2;
-        mot_LRRL_T = numTargets/4;
-        mot_RLLR_T = numTargets/4;
-        
-        % create the three chunks of condition and randomize them and check that:
-        % 1 - the target is not in the first trial
-        % 2 - two target are not consecutive
-        % 3 - there are no more them 3 same trials consecutive (less than that is impossible)
-        
-        d = 1; % counter for trial number
-        
-        while d < numEvents*2-2
-            % create a sequence of trials that contains 1 thirs of all the trials
-            trial_seq = [ ...
-                repmat(ones,1,static),  repmat(2,1,mot_LRRL),   repmat(3,1,mot_RLLR), ...
-                repmat(4,1,static_T),   repmat(5,1,mot_LRRL_T), repmat(6,1,mot_RLLR_T)];
-            
-            % we shuffle each chunck and concatenate them
-            trial_seq = [ Shuffle(trial_seq), Shuffle(trial_seq) ];
-            
-            % scan through the trial sequence and checks all the conditions
-            while d < length(trial_seq)-2
-                
-                if d == 1 && trial_seq(d) > 3 % no targets in the first trial
-                    d = 1;
-                    break
-                elseif trial_seq(d) > 3 && trial_seq(d+1) > 3 % avoid 2 consecutive targets
-                    d = 1;
-                    break
-                    % avoid series 3 times the same conditions
-                elseif trial_seq(d) ==  trial_seq(d+1) && trial_seq(d+1) ==  trial_seq(d+2) && trial_seq(d+2) ==  trial_seq(d+3)
-                    d = 1;                    
-                    break
-                else
-                    d = d+1;
-                end
-            end
-        end
+        trial_seq = pseudorand(numEvents, numTargets, nChunks, expLength);
         
 end
 
@@ -189,5 +54,66 @@ trial_seq_names(strcmpi(trial_seq_names,'3')) = {'mot_RLLR'};
 trial_seq_names(strcmpi(trial_seq_names,'4')) = {'static_T'};
 trial_seq_names(strcmpi(trial_seq_names,'5')) = {'mot_LRRL_T'};
 trial_seq_names(strcmpi(trial_seq_names,'6')) = {'mot_RLLR_T'};
+
+end
+
+function trial_seq = pseudorand(numEvents, numTargets, nChunks, expLength)
+
+% calculate how many trials per condition dividing them in 3 chunks to help
+% even randomization across experiment per condition
+
+static = (numEvents-numTargets)/2;
+mot_LRRL = (numEvents-numTargets)/4;
+mot_RLLR = (numEvents-numTargets)/4;
+static_T = numTargets/2;
+mot_LRRL_T = numTargets/4;
+mot_RLLR_T = numTargets/4;
+
+% create the three chunks of condition and randomize them and check that:
+% 1 - the target is not in the first trial
+% 2 - two target are not consecutive
+% 3 - there are no more them 3 same trials consecutive (less than that is impossible)
+
+d = 1; % counter for trial number
+
+while d < numEvents*nChunks-2
+    
+    % create a sequence of trials that contains 1 thirs of all the trials
+    trial_seq = [ ...
+        repmat(ones,1,static),  repmat(2,1,mot_LRRL),   repmat(3,1,mot_RLLR), ...
+        repmat(4,1,static_T),   repmat(5,1,mot_LRRL_T), repmat(6,1,mot_RLLR_T)];
+    
+    switch expLength
+        
+        case 1
+            
+            % we shuffle 3 times each chunck and concatenate them
+            trial_seq = [ Shuffle(trial_seq), Shuffle(trial_seq), Shuffle(trial_seq) ];
+            
+        otherwise
+            
+            % we shuffle 2 times each chunck and concatenate them
+            trial_seq = [ Shuffle(trial_seq), Shuffle(trial_seq) ];
+            
+    end
+    
+    % scan through the trial sequence and checks all the conditions
+    while d < length(trial_seq)-2
+        
+        if d == 1 && trial_seq(d) > 3 % no targets in the first trial
+            d = 1;
+            break
+        elseif trial_seq(d) > 3 && trial_seq(d+1) > 3 % avoid 2 consecutive targets
+            d = 1;
+            break
+            % avoid series 3 times the same conditions
+        elseif trial_seq(d) ==  trial_seq(d+1) && trial_seq(d+1) ==  trial_seq(d+2) && trial_seq(d+2) ==  trial_seq(d+3)
+            d = 1;
+            break
+        else
+            d = d+1;
+        end
+    end
+end
 
 end
