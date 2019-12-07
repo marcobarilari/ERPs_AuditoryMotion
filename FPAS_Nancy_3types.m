34217cbf044dbb961fba1eb918f1ee8650f3a347
Remi Gau <remi_gau@hotmail.com>
2019-12-07 18:02:37 +0100

    last commit before movign Franci script to another repo

--- FPAS_Nancy_3types.m
+++ FPAS_Nancy_3types.m
@@ -6,7 +6,9 @@ PsychPortAudio('Close');
 
 
 %% Parameters
-sub = 2; % subject code 
+SubjName = 2; % subject code 
+
+Run = 1;
 
 % Sequences types to be played: 1-standard, 2-scrambled, 6-harmonic
 seqType = [1 2 6]; 
@@ -28,24 +30,26 @@ FS = 44100; % Sampling frequency of the sounds
 % Create a folder for the subject code to save a text file with the order
 % of the sequences presented. If the folder of the subject code already
 % exists, it stops the script
-if exist((sprintf('SUB%03d_notes',sub)),'dir')==7
+if exist((sprintf('sub-%03d', SubjName)),'dir')==7
     warning('Folder already exist, stopping the script')
     return 
 else
-    mkdir(sprintf('SUB%03d_notes',sub));
+    mkdir(sprintf('sub-%03d', SubjName));
 end
 
 % Create a text file to save order sequence presentation
-txtFname = fullfile(sprintf('sub-%03d_notes', sub), sprintf('sub-%03d_list', sub) );
+txtFname = fullfile(sprintf('sub-%03d', SubjName), sprintf('log_sub-%03d_list.tsv', SubjName) );
 fidwrit = fopen(txtFname, 'wt');
+% add headers to log file
+fprintf(fidwrit, 'onset\tduration\ttrial_type\n');
 
 
 %% Loading sequences from defined folders
 for st = 1:length(seqType) %sequence type
     for cs = 1: nRep
         tyFo = sprintf('Type%d',seqType(st)); % type folder
-        suFo = sprintf('SUB%03d',sub); % subject folder
-        noFi = sprintf('Type%d_SUB%03d_%s.wav', seqType(st), sub,codeSeq{cs} ); % name of individual sequence
+        suFo = sprintf('SUB%03d',SubjName); % subject folder
+        noFi = sprintf('Type%d_SUB%03d_%s.wav', seqType(st), SubjName,codeSeq{cs} ); % name of individual sequence
         
         % save a structure called seq{seqType,codeSeq} 
         seq{st,cs}.name = noFi; %#ok<*SAGROW>
@@ -77,7 +81,7 @@ triggerSend('open', device, audio_config);
 
 %% Main loop
 % Loop over the number of repetition of each sound, so that each sequence type is presented at least one
-for i = 1:nRep 
+for iRep = 1:nRep 
     
     % (Pseudo)randomise order of presentation of the sequences (each
     % sequence type is presented once, then the randomisation is done again
@@ -85,13 +89,13 @@ for i = 1:nRep
     
     for j = 1:size(seq,1) % Loop over the different sequence types
         
+        
+        %% Wait space bar press
         % key code to start a sequence: 5
         keyCodeStart = KbName('5%');
-
         abletostart = false;
         keyIsDown = 0;
         
-        % Wait for trigger
         while ~abletostart 
             [keyIsDown, pressedSecs, keyCode] = KbCheck(-1);
             if keyIsDown
@@ -106,19 +110,18 @@ for i = 1:nRep
             end
         end
 
-        % save on the text file the sequence that is presented here
-        line = sprintf('%d\t%s',i,seq{orderseq(j),i}.name);
-        fprintf(fidwrit,'%s\n',line);
-
-        % Fill the audio playback buffer with audio data, doubled: stereo
-        sound = seq{orderseq(j),i}.audio;
+        %% Fill the audio playback buffer with audio data, doubled: stereo
+        sound = seq{orderseq(j),iRep}.audio;
         sound = [sound'; sound'];
-        
         audio_config.sound = sound;
         
         audio_config = triggerSend('fillBuffer', device, audio_config);
         
         audio_config = triggerSend('start', device, audio_config);
+        
+        if iRep==1
+            experimentStartTime = audio_config.playTime;
+        end
         
         % Check if the audio is being played or not
         status = PsychPortAudio('GetStatus', pahandle); 
@@ -128,26 +131,41 @@ for i = 1:nRep
         while status.Active==1
             status = PsychPortAudio('GetStatus', pahandle);
             [keyIsDown, pressedSecs, keyCode] = KbCheck(-1);
+            
             if keyIsDown
                 if find(keyCode)== KbName('DELETE') %% PUT ESCAPE HERE
+                    
                     % If the script is stopped while a sequence is being
                     % played, close psychport audio and send trigger
                     audio_config = triggerSend('start', device, audio_config);
                     return
                 elseif find(keyCode)== KbName('space')
+                    
                     % If space bar is pressed (attention task), trigger 5
                     % is sent
-                    sendparallelbyte(5); 
+%                     sendparallelbyte(5); 
+                    
                     while keyIsDown % Waits for space key to be released to continue
                         [keyIsDown, pressedSecs, keyCode] = KbCheck(-1);
                     end
-                    sendparallelbyte(0)
+                    
+%                     sendparallelbyte(0)
                 end
             end
+            
         end
-        
+     
 %         sendparallelbyte(trigCode+20);
 %         sendparallelbyte(0);
+
+
+        % Write to log file
+        iDuration = GetSecs - audio_config.playTime;
+        iOnset = audio_config.playTime - experimentStartTime;
+        iCondition = seq{orderseq(j),iRep}.name;
+        
+        line = sprintf('%f\t%f\t%iCondition\n', iOnset, iDuration, iCondition);
+
 
         % At the end of the sequence, end sequence is displayed on the
         % screen. After 5 seconds the next word appears and it is possible
@@ -165,3 +183,13 @@ fclose(fidwrit);
 %% At the end of the experiment:
 PsychPortAudio('Close');
 sca
+
+
+% [ t, table_header ] = make_events(SubjName, task_id, Run, onsets, durations, condition, ...
+%     names, ...
+%     target, ...
+%     Events_order, ...
+%     isi, ...
+%     eventEnd, ...
+%     response, ...
+%     responseTime);
