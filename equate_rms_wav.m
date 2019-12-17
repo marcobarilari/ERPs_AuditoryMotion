2a519f53a54d344b494c0b0750b30a6888b53618
Remi Gau <remi_gau@hotmail.com>
2019-12-18 00:16:04 +0100

    update after testing in nancy

--- equate_rms_wav.m
+++ equate_rms_wav.m
@@ -1,16 +1,14 @@
 function  equate_rms_wav()
 
 % input the folder with the audio files
-file_dir = '../../../Downloads/sEEG_experiments/SpeechTracking/combined/'; % 'stimuli/test/targets'
+file_dir = 'stimuli/test/targets'; % input_lepoulpe %input_earfish
 
 % read the wav file in the target folder
 addpath(fullfile(pwd, file_dir));
 file_list = dir([file_dir,'/*.wav']);
 
-
-stereo = 0; %set it to 1 if the .wav files are stereo
 % select the reference audio file
-reference_wav_fn = 'TR_mono_Orwell1984_11-31_12-22.wav';
+reference_wav_fn = 'mot_RL_2s.wav';
 
 % select only the audio file that are not rms-ed already and rms them 
 for i = 1:size(file_list,1)
@@ -19,7 +17,7 @@ for i = 1:size(file_list,1)
         
         target_wav_fn = file_list(i).name;
         fprintf('\n\n\n%s\n\n', target_wav_fn)
-        runFunction (reference_wav_fn,target_wav_fn,file_dir,stereo)
+        runFunction (reference_wav_fn,target_wav_fn,file_dir)
         
     end
 
@@ -27,7 +25,7 @@ end
 
 end
 
-function runFunction(reference_wav_fn,target_wav_fn,file_dir,stereo)
+function runFunction(reference_wav_fn,target_wav_fn,file_dir)
 %% This Script takes a file (target_wav_fn) and equates its rms with
 % another reference audio file (reference_wav_fn) amd gives the equated 
 % wav file as an output ('final_wave.wav')
@@ -45,12 +43,8 @@ disp('rms of the target wav file')
 disp(rms_target)
 
 % correct for the rms differences in each channel
-if stereo
-    final_wave = [ target_wav(:,1)*(rms_reference(1)/rms_target(1)) ...
-        target_wav(:,2)*(rms_reference(2)/rms_target(2))] ;
-else
-    final_wave = [ target_wav(:,1)*(rms_reference(1)/rms_target(1))] ;
-end
+final_wave = [ target_wav(:,1)*(rms_reference(1)/rms_target(1)) ...
+               target_wav(:,2)*(rms_reference(2)/rms_target(2))] ;
            
 % check that the rms of the final is similar to the original           
 rms_final = rms(final_wave);
@@ -64,17 +58,13 @@ figure()
 subplot(2,1,1)
 plot(reference_wav(:,1),'r')
 hold on 
-if stereo
-    plot(reference_wav(:,2),'b')
-end
+plot(reference_wav(:,2),'b')
 title('Reference wav file')
 
 subplot(2,1,2)
 plot(final_wave(:,1),'r')
 hold on 
-if stereo
-    plot(final_wave(:,2),'b')
-end
+plot(final_wave(:,2),'b')
 title('Final wav file')
 
 end
